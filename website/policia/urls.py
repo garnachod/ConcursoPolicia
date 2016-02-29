f8990886cebae3199380ee4ff8e93c0cff3b1ffa
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2016-02-29 13:59:53 +0100

    API REST búsqueda de similares. TODO: Sanitizar eval()

--- website/policia/urls.py
+++ website/policia/urls.py
@@ -4,11 +4,19 @@ from . import views
 app_name = 'policia'
 
 urlpatterns = [
+
+    # API REST
+    url(r'^api/buscar/similares/$', views.buscarSimilaresAPI, name='buscarSimilaresAPI'),
+
+    # Sistema de autenticacion de usuarios
     url(r'^login/$', views.login, name='login'),
     url(r'^login/(?P<code>[0-9]{1})/$', views.login, name='login'),
     url(r'^autenticar/$', views.autenticar, name='autenticar'),
     url(r'^salir/$', views.salir, name='salir'),
+
+    # Secciones del panel del control
     url(r'^buscar/similares/$', views.buscarSimilares, name='buscarSimilares'),
     url(r'^buscar/texto/$', views.buscarTexto, name='buscarTexto'),
     url(r'^$', views.tareas, name='tareas'),
+
 ]
--- website/policia/views.py
+++ website/policia/views.py
@@ -7,16 +7,88 @@ from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.utils import timezone
 import os
 import sys
+import math
 
 lib_path = os.path.abspath('../../')
 sys.path.append(lib_path)
 from API.APITextos import APITextos
+from .models import Tarea
 
 # IMPORTANTE: Abreviar cifras de seguidores/siguiendo
 # http://stackoverflow.com/questions/25611937/abbreviate-a-localized-number-in-javascript-for-thousands-1k-and-millions-1m
 
+#########################
+# API REST
+#########################
+
+def _formatTwitterFigure(n):
+    millnames = ['','K','M','B','T']
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
+def _formatUsersJson(usuarios):
+    usuarios = map((lambda user:
+        {
+            'name': user.name,
+            'screen_name': user.screen_name,
+            'followers': _formatTwitterFigure(user.followers),
+            'following': _formatTwitterFigure(user.following),
+            'location': user.location,
+            'created_at': user.created_at.strftime("%d/%m/%Y")
+        })
+        , usuarios)
+    return JsonResponse({ 'users': usuarios })
+
+@login_required
+def buscarSimilaresAPI(request):
+    try:
+        searchUsername = request.GET['search-username']
+        searchLanguage = request.GET['search-language']
+        searchMax = request.GET['search-max']
+        searchBy = request.GET['search-by']
+        searchIn = request.GET['search-in']
+    except Exception, e:
+        return JsonResponse("missing_params", safe=False)
+
+    tarea = Tarea(
+        usuario = request.user,
+        tipo = "user_" + searchIn + "_" + searchBy,
+        username = searchUsername,
+        idioma = searchLanguage,
+        num_usuarios = searchMax,
+        inicio = timezone.now(),
+        fin = None,
+        enviar_email = False
+    )
+    tarea.save()
+    username = searchUsername
+    language = searchLanguage
+    maxResults = int(searchMax)
+    idTarea = tarea.id
+
+    method = 'APITextos.getUsersSimilar_user_' + searchIn + '_' + searchBy + '(username, language, maxResults, idTarea)'
+
+    try:
+        result = eval(method)
+    except Exception, e:
+        result = []
+
+    if result == []:
+        return JsonResponse("no_results", safe=False)
+    elif result == False:
+        return JsonResponse("downloading", safe=False)
+    else:
+        return _formatUsersJson(result)
+
+#########################
+# Secciones del panel
+#########################
 @login_required
 def tareas(request):
     nombre = request.user.nombre + " " + request.user.apellidos
@@ -32,8 +104,6 @@ def tareas(request):
 def buscarSimilares(request):
     nombre = request.user.nombre + " " + request.user.apellidos
     email = request.user.email
-
-    """ APITextos.getUsersSimilar_user_all_topic('p_molins', 'es', 100, 1) """
 
     return render(request, "policia/buscar-similares.html", {
         'nombre': nombre,
@@ -51,6 +121,9 @@ def buscarTexto(request):
         'titulo': 'Búsqueda de usuarios por texto'
     })
 
+#########################
+# Sitema de autenticación
+#########################
 def login(request, code=None):
     if code == "0":
         error = "El usuario indicado fue deshabilitado."

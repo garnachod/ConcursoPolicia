bb2b82cb5fa91477afd58f8cea375edbf981638d
Daniel Garnacho <garnachod@gmail.com>
2017-01-14 21:43:51 +0100

    test write in file

--- external_tests/main.py
+++ external_tests/main.py
@@ -3,6 +3,6 @@ from FileTweetsWriter import FileTweetsWriter
 
 if __name__ == '__main__':
     writer = FileTweetsWriter("tweets.txt")
-    fetcher = RecolectorTweetsTagsLang(writer)
+    fetcher = RecolectorTweetsTagsLang([writer])
 
     fetcher.recolecta("consulta")
